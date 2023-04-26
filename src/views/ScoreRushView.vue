30330b4e07bef6cb0c9cd898c5d0275033acedfa
Tadija Tomić <ofah993@gmail.com>
2023-04-26 14:41:38 +0200

    dodavanje funkcionalnosti ScoreRush igri

--- src/views/ScoreRushView.vue
+++ src/views/ScoreRushView.vue
@@ -1,7 +1,77 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const result = ref(0)
+const btn = ref(null)
+
+const onHover = () => {
+  const maxX = window.innerWidth - btn.value.offsetWidth
+  const maxY = window.innerHeight - btn.value.offsetHeight
+  const currentX = btn.value.offsetLeft
+  const currentY = btn.value.offsetTop
+
+  let newX = Math.floor(Math.random() * maxX)
+  let newY = Math.floor(Math.random() * maxY)
+
+  if (newX < 0) {
+    newX = 0
+  } else if (newX > maxX) {
+    newX = maxX
+  }
+
+  if (newY < 0) {
+    newY = 0
+  } else if (newY > maxY) {
+    newY = maxY
+  }
+
+  btn.value.style.transform = `translate(${newX - currentX}px, ${newY - currentY}px)`
+}
+
+const onClick = () => {
+  onHover()
+  result.value++
+}
+</script>
 
 <template>
-  <h3>Score Rushhh</h3>
+  <div class="main">
+    <h1>{{ result }}</h1>
+    <button @mouseover="onHover" @click="onClick" ref="btn" class="btn">Catch me</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin: 5px 15px;
+}
+
+.main {
+  position: relative;
+  width: 1500px;
+  height: 670px;
+  cursor: grab;
+}
+.btn {
+  position: absolute;
+  width: 180px;
+  height: 85px;
+  padding: 10px;
+  background-color: #d82222;
+  border-radius: 17px;
+  outline: none;
+  color: #fafafa;
+  font-size: 2rem;
+  transition: transform 0.3s ease-in-out;
+  box-shadow: 4px 4px 45px rgba(176, 164, 164, 0.271);
+  user-select: none;
+  text-transform: uppercase;
+}
+
+.button-caught {
+  background-color: #02a238;
+  width: 105px;
+  height: 65px;
+  font-size: 1rem;
+}
+</style>
